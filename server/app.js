1c8adec6b06f8cc88de149e4a3e6b3708722d0fe
Santiago Villa <code@santiagovilla.it>
2025-05-29 18:54:22 +0200

    day 1

--- server/app.js
+++ server/app.js
@@ -5,7 +5,8 @@ const PORT = 5005;
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
-// ...
+const cohorts = require('./cohorts.json');
+const students = require('./students.json');
 
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
@@ -25,8 +26,20 @@ app.use(cookieParser());
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
+app.get("/", (req, res) => {
+  res.json(cohorts);
+});
+
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
+});
+
+app.get("/api/cohorts", (req, res) => {
+  res.json(cohorts);
+});
+
+app.get("/api/students", (req, res) => {
+  res.json(students);
 });
 
 
