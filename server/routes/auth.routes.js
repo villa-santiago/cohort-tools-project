eec25c5cf227936bc0b64e1611d3c08c0fdac3cc
Santiago Villa <code@santiagovilla.it>
2025-06-07 15:44:48 +0200

    deleted comment logs

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -50,7 +50,7 @@ router.post('/signup', (req, res, next) =>{
         res.status(201).json({user: user});
     })
     .catch(err =>{
-        console.log(err);
+        
         res.status(500).json({message:"Internal explosion"});
     });
 });
@@ -67,24 +67,22 @@ router.post('/login', (req, res, next) => {
     User.findOne({email})
     .then((foundUser) => {
         if(!foundUser){
-            console.log("User not found for email", email);
+            // console.log("User not found for email", email);
             res.status(401).json({message:"User not found"});
             return;
         }
 
-        console.log("Found user:", foundUser);
-        console.log("Stored hashed password:", foundUser.password);
+       
 
         const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
 
         if(passwordCorrect){
-            console.log("Password correct, creating token");
+            
 
             const{ _id, email, name} = foundUser;
             const payload = { _id, email, name};
 
-            console.log("JWT payload:", payload);
-            console.log("JWT secret from env:", process.env.TOKEN_SECRET);
+           
 
             const authToken = jwt.sign(
                 payload,
@@ -92,17 +90,17 @@ router.post('/login', (req, res, next) => {
                 {algorithm: 'HS256', expiresIn:"6h"}
             );
 
-            console.log("Auth token created successfully.");
+            
 
             res.status(200).json({authToken:authToken});
         }
         else {
-            console.log("Incorrect password for user:", email);
+            
             res.status(401).json({message:"Unable to authenticate"});
         }
     })
     .catch(err => {
-  console.error("Login error:", err);
+
   res.status(500).json({ message: "Auth error", error: err.message });
 });
 });
@@ -110,7 +108,7 @@ router.post('/login', (req, res, next) => {
 // GET /auth/verify
 router.get('/verify', isAuthenticated, (req, res, next) => {
     
-    console.log('req.payload', req.payload);
+   
 
     res.status(200).json(req.payload);
 
